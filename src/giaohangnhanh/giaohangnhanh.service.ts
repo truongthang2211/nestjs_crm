29a200997de07669357272420ad7bc1489b31b23
Thang Nguyen Anh <truongthang2211@gmail.com>
2023-01-02 19:07:07 +0700

    Update cors

--- src/giaohangnhanh/giaohangnhanh.service.ts
+++ src/giaohangnhanh/giaohangnhanh.service.ts
@@ -15,9 +15,7 @@ export class GiaohangnhanhService {
       method: 'get',
       url: `https://b24-aypxm5.bitrix24.vn/rest/1/0tinjuv7qbs1si9g/crm.deal.get.json?id=${DealID}`,
     };
-    let dataRs: any = await lastValueFrom(
-      this.httpService.request(config),
-    ).catch((e) => console.log(e));
+    let dataRs: any = await lastValueFrom(this.httpService.request(config));
     return dataRs.data.result;
   }
   async getInfOBuyer(contactID: number): Promise<Contact> {
@@ -25,9 +23,7 @@ export class GiaohangnhanhService {
       method: 'get',
       url: `https://b24-aypxm5.bitrix24.vn/rest/1/x6a2dlkxklx8n3np/crm.contact.get.json?id=${contactID}`,
     };
-    let dataRs: any = await lastValueFrom(
-      this.httpService.request(config),
-    ).catch((e) => console.log(e));
+    let dataRs: any = await lastValueFrom(this.httpService.request(config));
     console.log(dataRs.data);
     return dataRs.data.result;
   }
@@ -36,59 +32,59 @@ export class GiaohangnhanhService {
       method: 'get',
       url: `https://b24-aypxm5.bitrix24.vn/rest/1/uehey2x4vjrxcz4g/crm.deal.productrows.get.json?id=${DealID}`,
     };
-    let dataRs: any = await lastValueFrom(
-      this.httpService.request(config),
-    ).catch((e) => console.log(e));
+    let dataRs: any = await lastValueFrom(this.httpService.request(config));
     return dataRs.data.result;
   }
   async create(dealID: number) {
-    let products = await this.getProductList(dealID);
-    let deal = await this.getDeal(dealID);
-    let contact = await this.getInfOBuyer(Number(deal.CONTACT_ID));
-    var data = JSON.stringify({
-      to_name: contact.NAME,
-      payment_type_id: 2,
-      to_phone: contact.PHONE[0]?.VALUE,
-      required_note: 'CHOXEMHANGKHONGTHU',
-      to_address: contact.ADDRESS ?? 'Đường Hàn Thuyên',
-      client_order_code: String(dealID),
-      weight: 12,
-      to_ward_name: contact.ADDRESS_PROVINCE ?? 'Linh Trung',
-      cod_amount: 200000,
-      to_district_name: contact.ADDRESS_REGION ?? 'Thủ Đức',
-      to_province_name: contact.ADDRESS_CITY ?? 'TP Hồ Chí Minh',
-      length: 1,
-      width: 19,
-      height: 10,
-      service_id: 53320,
-      items: products.map((p) => {
-        return {
-          name: p.PRODUCT_NAME,
-          code: String(p.PRODUCT_ID),
-          quantity: p.QUANTITY,
-          price: p.PRICE,
-          length: 12,
-          width: 12,
-          height: 12,
-        };
-      }),
-    });
-    console.log(data);
+    try {
+      let products = await this.getProductList(dealID);
+      let deal = await this.getDeal(dealID);
+      let contact = await this.getInfOBuyer(Number(deal.CONTACT_ID));
+      var data = JSON.stringify({
+        to_name: contact.NAME,
+        payment_type_id: 2,
+        to_phone: contact.PHONE[0]?.VALUE,
+        required_note: 'CHOXEMHANGKHONGTHU',
+        to_address: contact.ADDRESS ?? 'Đường Hàn Thuyên',
+        client_order_code: String(dealID),
+        weight: 12,
+        to_ward_name: contact.ADDRESS_PROVINCE ?? 'Linh Trung',
+        cod_amount: 200000,
+        to_district_name: contact.ADDRESS_REGION ?? 'Thủ Đức',
+        to_province_name: contact.ADDRESS_CITY ?? 'TP Hồ Chí Minh',
+        length: 1,
+        width: 19,
+        height: 10,
+        service_id: 53320,
+        items: products.map((p) => {
+          return {
+            name: p.PRODUCT_NAME,
+            code: String(p.PRODUCT_ID),
+            quantity: p.QUANTITY,
+            price: p.PRICE,
+            length: 12,
+            width: 12,
+            height: 12,
+          };
+        }),
+      });
+      console.log(data);
 
-    var config = {
-      method: 'post',
-      url: 'https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/create',
-      headers: {
-        'Content-Type': 'application/json',
-        ShopId: '121254',
-        Token: 'aaceecdc-85f7-11ed-8183-12cf3da973bf',
-      },
-      data: data,
-    };
-    let dataRs: any = await lastValueFrom(
-      this.httpService.request(config),
-    ).catch((e) => console.log(e));
-    return dataRs.data;
+      var config = {
+        method: 'post',
+        url: 'https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/create',
+        headers: {
+          'Content-Type': 'application/json',
+          ShopId: '121254',
+          Token: 'aaceecdc-85f7-11ed-8183-12cf3da973bf',
+        },
+        data: data,
+      };
+      let dataRs: any = await lastValueFrom(this.httpService.request(config));
+      return dataRs.data;
+    } catch (error) {
+      return error;
+    }
   }
 
   findAll() {
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import * as csurf from 'csurf';
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: false });
+  app.enableCors();
+  await app.listen(3001);
 }
 bootstrap();
